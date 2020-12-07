1eb8ac1bf2428c2869456d25335aafaed13a2157
emnbdx <eddy.montus@gmail.com>
2020-12-07 23:28:51 +0100

    add github ribbon

--- index.php
+++ index.php
@@ -49,9 +49,17 @@
         border-radius: 1em;
         box-shadow: 0 1px 4px rgba(0, 0, 0, 0.5);
       }
+
+      .github {
+        float: right;
+      }
     </style>
   </head>
   <body>
+    <a href="https://github.com/emnbdx/spreadthelove" class="github">
+      <img loading="lazy" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1">
+    </a>
+
     <div class="container h-100">
       <div class="row h-100">
         <div class="form-container col-12 my-auto">
